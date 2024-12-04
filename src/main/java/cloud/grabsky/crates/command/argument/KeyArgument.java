2ab337f6273d0e9aa75be4d0d7bc94934be633dd
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2024-12-04 13:56:00 +0100

    relicense under GPLv3

--- src/main/java/cloud/grabsky/crates/command/argument/KeyArgument.java
+++ src/main/java/cloud/grabsky/crates/command/argument/KeyArgument.java
@@ -1,3 +1,17 @@
+/*
+ * Crates (https://github.com/Grabsky/Crates)
+ *
+ * Copyright (C) 2024  Grabsky <michal.czopek.foss@proton.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3 for more details.
+ */
 package cloud.grabsky.crates.command.argument;
 
 import cloud.grabsky.bedrock.components.Message;
